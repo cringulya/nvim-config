7dee607d117f3837a947477312f4089d02ec9c62
Artem Son <s0sis0n@yandex.ru>
2025-02-27 14:08:33 +0300

    updates

--- init.lua
+++ init.lua
@@ -4,6 +4,6 @@ require('keybinds')
 require('gui-settings')
 require('filetype')
 
-vim.cmd([[colorscheme rose-pine]])
--- vim.cmd([[colorscheme gruvbox]])
+-- vim.cmd([[colorscheme rose-pine]])
+vim.cmd([[colorscheme tokyonight]])
 -- require('kanagawa').load('wave')
