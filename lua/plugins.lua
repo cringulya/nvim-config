642c7bcb1100db88fae63f4c5aea02e75d61d705
Artem Son <artemson@abobus-mb.local>
2025-02-27 22:25:57 +0300

    updates

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -461,7 +461,7 @@ require('lazy').setup({
     'lervag/vimtex',
     ft = 'tex',
     config = function()
-      vim.g.vimtex_compiler_latexmk_engines = { _ = '-pdflatex -shell-escape' }
+      vim.g.vimtex_compiler_latexmk_engines = { _ = '-lualatex -shell-escape' }
       vim.g.vimtex_view_method = 'zathura'
       vim.g.vimtex_compiler_latexmk = {
         out_dir = './build/',
